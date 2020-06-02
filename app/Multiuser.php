2e7cf8b3b7959b8e2db5abf40de054df81e8e7b3
theaf1 <sophon.nit@mahidol.ac.th>
2020-06-02 16:36:36 +0700

    add relationship code

--- app/Multiuser.php
+++ app/Multiuser.php
@@ -10,8 +10,19 @@ class Multiuser extends Model
         'id',
         'name',
     ];
+    //แสดงความสัมพันธ์กับตาราง Client
     public function MultiUserClient ()
     {
         return $this->hasMany(Client::class,'multi_user_id');
+    }
+    //แสดงความสัมพันธ์กับตาราง Peripheral
+    public function MultiUserPeripheral ()
+    {
+        return $this->hasMany(Peripheral::class);
+    }
+    //แสดงความสัมพันธ์กับตาราง Storageperipherals
+    public function MultiUserStoragePeripheral ()
+    {
+        return $this->hasMany(Storageperipherals::class);
     }
 }
--- app/Position.php
+++ app/Position.php
@@ -11,8 +11,19 @@ class Position extends Model
         'name',
     ];
 
+    //แสดงความสัมพันธ์กับตาราง Client
     public function PositionClient ()
     {
         return $this->hasMany(Client::class,'position_id');
+    }
+    //แสดงความสัมพันธ์กับตาราง Peripheral
+    public function PositionPeripheral ()
+    {
+        return $this->hasMany(Peripheral::class);
+    }
+    //แสดงความสัมพันธ์กับตาราง Storageperipheral
+    public function PositionStoragePeripherals ()
+    {
+        return $this->hasMany(Storageperipherals::class);
     }
 }
