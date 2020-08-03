6c17e1a3cff85bd6907d50cf94e766dc9380a8d6
theaf1 <sophon.nit@mahidol.ac.th>
2020-08-03 10:35:47 +0700

    grouped routes

--- routes/web.php
+++ routes/web.php
@@ -29,45 +29,45 @@ Route::post('/store', 'SectionController@store');
 
 Route::get('/computer', 'ClientController@index');
 Route::post('/add-computer', 'ClientController@store');
+Route::get('/client', 'ClientIndexController@index');
 Route::get('/client/{id}','ClientController@edit');
 Route::put('/client/{id}/update','ClientController@update');
 
 Route::get('/peripherals','PeripheralsController@index');
 Route::post('/add-peripheral','PeripheralsController@store');
+Route::get('/peripheral', 'PeripheralsIndexController@index');
 Route::get('/peripheral/{id}', 'PeripheralsController@edit');
 Route::put('peripheral/{id}', 'PeripheralsController@update');
 
 Route::get('/storage', 'StorageperipheralsController@index');
 Route::post('/add-sp','StorageperipheralsController@store');
+Route::get('/storageperipheral', 'StorageperipheralsIndexController@index');
 Route::get('/storageperipheral/{id}', 'StorageperipheralsController@edit');
 Route::put('/storageperipheral/{id}', 'StorageperipheralsController@update');
 
 Route::get('/network', 'NetworkdeviceController@index');
 Route::post('/add-networkdev','Networkdevicecontroller@store');
+Route::get('/networkdevices', 'NetworkdeviceIndexController@index');
 Route::get('/networkdevices/{id}', 'NetworkdeviceController@edit');
 Route::put('/networkdevices/{id}', 'NetworkdeviceController@update');
 
 Route::get('/ups', 'UpsController@index');
 Route::post('/add-ups',"UpsController@store");
+Route::get('/upses', 'UpsIndexController@index');
 Route::get('/ups/{id}', 'Upscontroller@edit');
 Route::put('/ups/{id}', 'Upscontroller@update');
 
 Route::get('/ns', 'NetworkedstorageController@index');
 Route::post('/add-ns','NetworkedstorageController@store');
+Route::get('/networkedstorage', 'NetworkedstorageIndexController@index');
 Route::get('/networkedstorage/{id}', 'Networkedstoragecontroller@edit');
 Route::put('/networkedstorage/{id}', 'Networkedstoragecontroller@update');
 
 Route::get('/server','ServerController@index');
 Route::post('/add-server','ServerController@store');
+Route::get('/servers', 'ServerIndexController@index');
 Route::get('/server/{id}', 'ServerController@edit');
 Route::put('/server/{id}', 'ServerController@update');
 //under development
 Route::get('/admin', 'SectionController@index');
 Route::get('/search', 'SearchController@index');
-Route::get('/client', 'ClientIndexController@index');
-Route::get('/peripheral', 'PeripheralsIndexController@index');
-Route::get('/storageperipheral', 'StorageperipheralsIndexController@index');
-Route::get('/servers', 'ServerIndexController@index');
-Route::get('/networkdevices', 'NetworkdeviceIndexController@index');
-Route::get('/networkedstorage', 'NetworkedstorageIndexController@index');
-Route::get('/upses', 'UpsIndexController@index');
