4293af962d1af98c6e5e5eff9fc2e2dd35c1cfc0
theaf1 <sophon.nit@mahidol.ac.th>
2020-08-17 15:39:02 +0700

    work on search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,71 +2,31 @@
 @section('content')
     <div class="container-fluid">
         <div class="col-12 mx-auto">
-            <form>
+            <form action="post">
                 <div class="form-row">
                     <div class="col-sm-12 col-lg-6">
                         <div class="form-group">
                             <label for="search_class">ต้องการค้นหา</label>
                             <select name="a" id="search_class" class="form-control">
                                 <option value="" hidden></option>
-                                <optgroup label="client">
-                                    @foreach ($clienttypes as $clienttype)
-                                        <option value="{{ $clienttype['id'] }}">{{ $clienttype['name'] }}</option>
-                                    @endforeach
-                                </optgroup>
-                                <optgroup label="peripherals">
-                                    @foreach ($peripheraltypes as $peripheraltype)
-                                        <option value="{{ $peripheraltype['id'] }}">{{ $peripheraltype['name'] }}</option>
-                                    @endforeach
-                                    <option value="c">storage device</option>
-                                </optgroup>
-                                <optgroup label="infrastructure">
-                                    <option value="d">server</option>
-                                    @foreach ($netsubtypes as $netsubtype)
-                                        <option value="{{ $netsubtype['id'] }}">{{ $netsubtype['name'] }}</option>
-                                    @endforeach
-                                    <option value="f">networked storage</option>
-                                    <option value="g">UPS</option>
-                                </optgroup>
+                                <option value="1">เครื่องคอมพิวเตอร์</option>
+                                <option value="2">อุปกรณ์ต่อพ่วง</option>
+                                <option value="3">อุปกรณ์ต่อพ่วงเก็บข้อมูล</option>
+                                <option value="4">อุปกรณ์เครือข่าย</option>
+                                <option value="5">เครื่องคอมพิวเตอร์แม่ข่าย</option>
+                                <option value="6">อุปกรณ์เก็บข้อมูลเครือข่าย</option>
+                                <option value="7">เครื่องสำรองไฟฟ้า</option>
                             </select>
                         </div>
                     </div>
                     <div class="col-sm-12 col-lg-6">
                         <div class="form-group">
-                            <label for="property">มีคุณสมบัติ</label>
-                            <select name="search_property" id="property" class="form-control">
+                            <label for="">ด้วย</label>
+                            <select name="" id="" class="form-control">
                                 <option value="" hidden></option>
-                                <option value="1">thing1</option>
-                                <option value="2">thing2</option>
-                                <option value="3">thing3</option>
+                                <option value="1">รหัส SAP</option>
+                                <option value="2">รหัสครุภัณฑ์</option>
                             </select>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col-sm-12 col-lg-6">
-                        <div class="form-group">
-                            <label for="search_arg">ด้วย</label>
-                            <input type="search" name="search_arg" id="search_arg" class="form-control">
-                        </div>
-                    </div>                
-                    <div class="col-sm-12 col-lg-6">
-                        <div class="form-group">
-                            <label for="ops"></label>
-                            <select class="form-control" id="ops">
-                                <option value="1">></option>
-                                <option value="2"><</option>
-                                <option value="3">=</option>
-                                <option value="4">>=</option>
-                                <option value="5"><=</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col-sm-12 col-lg-6">
-                        <div class="form-group">
-                            <input type="datetime-local" class="form-control">
                         </div>
                     </div>
                 </div>
