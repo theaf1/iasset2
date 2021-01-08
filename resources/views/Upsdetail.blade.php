e380434f6afd5eb3d26c38e70b63394f67de2e8b
= <=>
2021-01-08 10:13:02 +0700

    finished UPS detail page

--- resources/views/Upsdetail.blade.php
+++ resources/views/Upsdetail.blade.php
@@ -69,11 +69,92 @@
             </div>
             <div class="card mt-4">
                 <div class="card-header card-background text-white">
-                    <h4>xxx</h4>
+                    <h4>รายละเอียดของครุภัณฑ์</h4>
                 </div>
                 <div class="card-body">
-
+                    <div class="form-row">
+                        <div class="col-sm-12 col-lg-6">
+                            <div class="form-group">
+                                <p>ยี่ห้อ: {{ $ups['brand'] }}</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-6">
+                            <div class="form-group">
+                                <p>รุ่น: {{ $ups['model'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-sm-12 col-lg-6">
+                            <div class="form-group">
+                                <p>serial number: {{ $ups['serial_no'] }}</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-6">
+                            <div class="form-group">
+                                <p>ลัษณะตัวเครื่อง: {{ $ups['form_factor'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-sm-12 col-lg-6">
+                            <div class="form-group">
+                                <p>หลักการทำงาน: {{$ups['topology']}}</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-6">
+                            <p>กำลังไฟสูงสุด {{ $ups['capacity'] }} KVA</p>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-sm-4 col-lg-3">
+                            <div class="form-group">
+                                <p>เปลี่ยนกำลังไฟสูงสุด{{ $ups['is_modular'] == 1 ? 'ได้' : 'ไม่ได้' }}</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-lg-3">
+                            <div class="form-group">
+                                <p>ใช้แบตเตอรี่{{$ups['battery_type']}}</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-lg-3">
+                            <div class="form-group">
+                                <p>{{ $ups['has_external_battery'] == 1 ? 'มี' : 'ไม่มี' }}แบตเตอรี่ภายนอก</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-sm-12 col-lg-6">
+                            <div class="form-group">
+                                <p>IP Address ที่ใช้ควบคุมเครื่อง: {{ $ups['device_management_address'] }}</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-header card-background text-white">
+                    <h4>หมายเหตุและปัญหาในการใช้งาน</h4>
+                </div>
+                <div class="card-body">
+                    <div class="form-row">
+                        <div class="col-sm-12 col-lg-6">
+                            <div class="form-group">
+                                <label for="issues">ปัญหาในการใช้งาน</label><br>
+                                {{ $ups['issues'] }}
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-6">
+                            <div class="form-group">
+                                <label for="remarks">หมายเหตุ</label><br>
+                                {{ $ups['remarks'] }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center mt-4">
+                <a href="{{url('/upses')}}" class="btn btn-primary btn-lg btn-block" role="button">ย้อนกลับ</a>
             </div>
         </div>
     </div>
