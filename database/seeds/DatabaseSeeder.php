30a73650479d5eb64fcbfe4134c1202414628620
= <=>
2021-01-19 11:27:21 +0700

    updated database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run() //เรียก seeder ที่ต้องใช้งาน
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(UpsSeeder::class); //seed ตาราง Upses
+        // $this->call(UsersTableSeeder::class); 
+        $this->call(StorageperipheralSeeder::class); //Seed ตาราง Storageperipherals
         $this->call(NetworkDeviceSeeder::class); //seed ตาราง Networkdevice
-        $this->call(StorageperipheralSeeder::class); //Seed ตาราง Storageperipherals 
+        $this->call(NetworkedStorageSeeder::class);
+        $this->call(UpsSeeder::class); //seed ตาราง Upses 
     }
 }
