a3fbeeae4187fe9eeab2057eea95d005a92bf84d
theaf1 <sophon.nit@mahidol.ac.th>
2021-05-03 10:29:55 +0700

    adjusted menu name

--- resources/views/adminmenus.blade.php
+++ resources/views/adminmenus.blade.php
@@ -5,12 +5,12 @@
             <div class="card-deck">
                 <div class="card mt-4 mr-2">
                     <div class="card-header card-background text-white">
-                        <p>1</p>
+                        <p>จัดการข้อมูลส่วนกลาง</p>
                     </div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-sm-12 col-lg-6">
-                                <a href="{{url('/sectionadmin')}}" class="btn btn-info btn-block mt-4" role="button">add section</a>
+                                <a href="{{url('/sectionadmin')}}" class="btn btn-info btn-block mt-4" role="button">จัดการข้อมูลหน่วยงาน</a>
                                 <a href="#" class="btn btn-info btn-block mt-4" role="button">add section</a>
                             </div>
                             <div class="col-sm-12 col-lg-6">
