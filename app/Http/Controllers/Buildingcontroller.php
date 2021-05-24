0dfdf7f61f56a906ba0863ca5264506f45bb83ca
theaf1 <sophon.nit@mahidol.ac.th>
2021-05-24 11:11:05 +0700

    fix typo

--- app/Http/Controllers/Buildingcontroller.php
+++ app/Http/Controllers/Buildingcontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Building;
 use Illuminate\Http\Request;
 
-class Buildingcontroller extends Controller
+class BuildingController extends Controller
 {
     /**
      * Display a listing of the resource.
