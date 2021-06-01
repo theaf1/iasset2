d73bf9429f1ddc421b050d9f3c435e7090435c67
theaf1 <sophon.nit@mahidol.ac.th>
2021-06-01 15:27:40 +0700

    add commentary

--- app/building.php
+++ app/building.php
@@ -7,18 +7,21 @@ use Laravel\Scout\Searchable;
 
 class Building extends Model
 {
-    use Searchable;
+    use Searchable; // กำหนด column ที่สามารถเปลี่ยนแปลงข้อมูลได้
     protected $fillable = [
         'id',
         'name'
     ];
     
+    //อ่าข้อมูลจากไฟล์ csv
     public static function loadData($fileName){
         $buildingRecords = loadCSV($fileName);
         foreach($buildingRecords as $buildingRecord){
             Building::create($buildingRecord);
         }
     }
+
+    //แสดงความสัมพันธกับตาราง Location
     public function BuildingLocation () {
         return $this->hasMany(Location::class,'building_id');
     }
