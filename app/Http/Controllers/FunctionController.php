c5e54c02030e963e7ef25845367a73dacda6625f
theaf1 <sophon.nit@mahidol.ac.th>
2021-06-15 11:28:28 +0700

    start adding admin to opsfunction model

---
+++ app/Http/Controllers/FunctionController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Opsfunction;
+use Illuminate\Http\Request;
+
+class FunctionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $Functions = Opsfunction::all();
+        return view('/admin/functionindex')->with([
+            'functions'=>$Functions,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('/admin/addfunction');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validateData($request);
+        $Functions = Opsfunction::create($request->all());
+        return redirect('/admin/functionadmin')->with('success','a');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $Function = Opsfunction::find($id);
+        return view('/admin/editfunction')->with([
+            'function'=>$Function,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validateData($request);
+        Opsfunction::find($id)->update($request->all());
+        return redirect('/admin/functionadmin')->with('success','bb');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    private function validateData($data)
+    {
+        $rules = [
+            'name'=>'required|unique:App\Opsfunction,name',
+        ];
+
+        $messages = [
+            'name.required'=>'aa',
+            'name.unique'=>'aaa',
+        ];
+
+        return $this->validate($data, $rules, $messages);
+    }
+}
---
+++ resources/views/admin/functionadmin.blade.php
@@ -0,0 +1,54 @@
+@extends('Layouts.app')
+@section('name')
+    <div class="container-fluid">
+        <div class="col-12 mx-auto">
+            <div class="modal fade" id="alert" tabindex="-1" role="dialog">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">ข้อความจากระบบ</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        </div>
+                        <div class="modal-body">
+                            @if(Session::has('success'))
+                                {{ Session::get('success') }} 
+                            @endif
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">รับทราบ</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-header card-background text-white">
+                    <h4>บัญชีรายชื่อระบบงานภายใน</h4>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="btn btn-primary btn-block" role="button">เพิ่มระบบงานภายใน</a>
+                    <table class="table table-hover table-responsive mt-4">
+                        <thead>
+                            <tr>
+                                <th scope="col">ลำดับที่</th>
+                                <th scope="col">ชื่อ</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($functions as $function)
+                                <tr>
+                                    <th scope="row">{{$function['id']}}</th>
+                                    <td>{{$function['name']}}</td>
+                                    <td><a href="#" class="btn btn-primary btn-sm" role="button">แก้ไข</a></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <div class="text-center mt-4">
+                        <a href="{{url('/admin')}}" class="btn btn-secondary btn-block" role="button">กลับไปเมนูผู้ดูแลระบบ</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
