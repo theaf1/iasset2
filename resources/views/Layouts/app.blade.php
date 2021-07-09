fc37ed99d0cde4a3f5ddcdd23defddd8e34b5764
theaf1 <sophon.nit@mahidol.ac.th>
2021-07-09 10:27:43 +0700

    improved main menu on navbar

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -18,39 +18,46 @@
   <body>
     @section('header')
       <nav class="navbar navbar-expand-lg navbar-light" style="background-color:808080;">
-      <a class="navbar-brand text-white" href="{{ url('/') }}">ระบบติดตามครุภัณฑ์คอมพิวเตอร์</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <a class="navbar-brand text-white" href="{{ url('/') }}">ระบบติดตามครุภัณฑ์คอมพิวเตอร์</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              เมนูหลัก
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="{{ url('/computer') }}">เพิ่มข้อมูลคอมพิวเตอร์</a>
-              <a class="dropdown-item" href="{{ url('/peripherals') }}">เพิ่มข้อมูลอุปกรณต่อพ่วง</a>
-              <a class="dropdown-item" href="{{ url('/storage') }}">เพิ่มข้อมูลอุปกรณต่อพ่วงเก็บข้อมูล</a>
-              <a class="dropdown-item" href="{{ url('/server') }}">เพิ่มข้อมูลคอมพิวเตอร์แม่ข่าย</a>
-              <a class="dropdown-item" href="{{ url('/network') }}">เพิ่มข้อมูลอุปกรณเครือข่าย</a>
-              <a class="dropdown-item" href="{{ url('/ns') }}">เพิ่มข้อมูลอุปกรณ์เก็บข้อมูลเครือข่าย</a>
-              <a class="dropdown-item" href="{{ url('/ups') }}">เพิ่มข้อมูลเครื่องสำรองไฟฟ้า</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="{{ url('/client') }}">แก้ไขข้อมูลคอมพิวเตอร์</a>
-              <a class="dropdown-item" href="{{ url('/peripheral') }}">แก้ไขข้อมูลอุปกรณ์ต่อพ่วง</a>
-              <a class="dropdown-item" href="{{ url('/storageperipheral') }}">แก้ไขข้อมูลอุปกรณ์ต่อพ่วงเก็บข้อมูล</a>
-              <a class="dropdown-item" href="{{ url('/servers') }}">แก้ไขข้อมูลคอมพิวเตอร์แม่ข่าย</a>
-              <a class="dropdown-item" href="{{ url('/networkdevices') }}">แก้ไขข้อมูลอุปกรณเครือข่ายคอมพิวเตอร์</a>
-              <a class="dropdown-item" href="{{ url('/networkedstorage') }}">แก้ไขข้อมูลอุปกรณ์เก็บข้อมูลเครือข่าย</a>
-              <a class="dropdown-item" href="{{ url('/upses') }}">แก้ไขข้อมูลเครื่องสำรองไฟฟ้า</a>
-              <div class="dropdown-divider"></div>
-              <a href="{{url('/admin')}}" class="dropdown-item">สำหรับผู้ดูแลระบบ</a>
-            </div>
-          </li>
-        </ul>
-      </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                เมนูหลัก
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ url('/computer') }}">เพิ่มข้อมูลคอมพิวเตอร์</a>
+                <a class="dropdown-item" href="{{ url('/peripherals') }}">เพิ่มข้อมูลอุปกรณต่อพ่วง</a>
+                <a class="dropdown-item" href="{{ url('/storage') }}">เพิ่มข้อมูลอุปกรณต่อพ่วงเก็บข้อมูล</a>
+                <a class="dropdown-item" href="{{ url('/server') }}">เพิ่มข้อมูลคอมพิวเตอร์แม่ข่าย</a>
+                <a class="dropdown-item" href="{{ url('/network') }}">เพิ่มข้อมูลอุปกรณเครือข่าย</a>
+                <a class="dropdown-item" href="{{ url('/ns') }}">เพิ่มข้อมูลอุปกรณ์เก็บข้อมูลเครือข่าย</a>
+                <a class="dropdown-item" href="{{ url('/ups') }}">เพิ่มข้อมูลเครื่องสำรองไฟฟ้า</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                เมนูรอง
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ url('/client') }}">แก้ไขข้อมูลคอมพิวเตอร์</a>
+                <a class="dropdown-item" href="{{ url('/peripheral') }}">แก้ไขข้อมูลอุปกรณ์ต่อพ่วง</a>
+                <a class="dropdown-item" href="{{ url('/storageperipheral') }}">แก้ไขข้อมูลอุปกรณ์ต่อพ่วงเก็บข้อมูล</a>
+                <a class="dropdown-item" href="{{ url('/servers') }}">แก้ไขข้อมูลคอมพิวเตอร์แม่ข่าย</a>
+                <a class="dropdown-item" href="{{ url('/networkdevices') }}">แก้ไขข้อมูลอุปกรณเครือข่ายคอมพิวเตอร์</a>
+                <a class="dropdown-item" href="{{ url('/networkedstorage') }}">แก้ไขข้อมูลอุปกรณ์เก็บข้อมูลเครือข่าย</a>
+                <a class="dropdown-item" href="{{ url('/upses') }}">แก้ไขข้อมูลเครื่องสำรองไฟฟ้า</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a href="{{url('/admin')}}" class="nav-link text-white">สำหรับผู้ดูแลระบบ</a>
+            </li> 
+          </ul>
+        </div>
       </nav>
     @yield('content')
 </body>
