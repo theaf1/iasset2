73a501d5ec87fb05e8895a6b0ad156aef1a07504
theaf1 <sophon.nit@mahidol.ac.th>
2021-07-13 11:04:42 +0700

    fix typo on navbar

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -41,7 +41,7 @@
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                แก้ไชครุภัณฑ์
+                แก้ไขข้อมูลครุภัณฑ์
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="{{ url('/client') }}">แก้ไขข้อมูลคอมพิวเตอร์</a>
