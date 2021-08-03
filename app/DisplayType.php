3546acd8a8ebfed968f72554f0d747de4c88e9ed
theaf1 <sophon.nit@mahidol.ac.th>
2021-08-03 10:16:13 +0700

    created DisplayType model

---
+++ app/DisplayType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DisplayType extends Model
+{
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+}
---
+++ database/migrations/2021_08_03_095330_create_display_types_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\DisplayType;
+
+class CreateDisplayTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('display_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+        $DisplayTypes = array(
+            ['name'=>'CRT'],
+            ['name'=>'LCD'],
+        );
+        foreach ($DisplayTypes as $DisplayType)
+        {
+            DisplayType::create($DisplayType);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('display_types');
+    }
+}
