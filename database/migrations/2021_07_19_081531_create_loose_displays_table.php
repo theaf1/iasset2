2d2740c7195e9a49605e157ce237cac52bb2083e
theaf1 <sophon.nit@mahidol.ac.th>
2021-09-10 14:18:44 +0700

    added commentary

--- database/migrations/2021_07_19_081531_create_loose_displays_table.php
+++ database/migrations/2021_07_19_081531_create_loose_displays_table.php
@@ -15,29 +15,29 @@ class CreateLooseDisplaysTable extends Migration
     {
         Schema::create('loose_displays', function (Blueprint $table) {
             $table->id();
-            $table->string('sapid');
-            $table->string('pid')->nullable();
-            $table->integer('owner_id');
-            $table->integer('mobility_id');
-            $table->integer('location_id');
-            $table->integer('section_id');
-            $table->string('response_person');
-            $table->integer('position_id');
-            $table->string('tel_no');
-            $table->integer('asset_status_id');
-            $table->integer('asset_use_status_id');
-            $table->string('brand');
-            $table->string('model');
-            $table->string('serial_no');
-            $table->integer('display_type_id');
-            $table->integer('display_size');
-            $table->integer('display_ratio_id');
-            $table->boolean('is_vga')->default(0);
-            $table->boolean('is_dvi')->default(0);
-            $table->boolean('is_hdmi')->default(0);
-            $table->boolean('is_dp')->default(0);
-            $table->string('issues')->nullable();
-            $table->string('remarks')->nullable();
+            $table->string('sapid'); //รหัส SAP
+            $table->string('pid')->nullable(); //รหัสครุภัณฑ์
+            $table->integer('owner_id'); //เจ้าของ รับค่าจากตาราง owner
+            $table->integer('mobility_id'); //ค่า mobility_id รับค่าจากตาราง Mobility
+            $table->integer('location_id'); //ค่า location_id รับค่าจากตาราง Room
+            $table->integer('section_id'); //หน่วยงาน รับค่าจากตาราง Section
+            $table->string('response_person'); //ผู้รับผิดชอบ
+            $table->integer('position_id'); //ตำแหน่ง รับค่าจากตาราง Position
+            $table->string('tel_no'); //หมายเลขโทรศัพท์
+            $table->integer('asset_status_id'); //สถานะทางทะเบียนครุภัณฑ์ รับค่าจากตาราง Asset_statuses
+            $table->integer('asset_use_status_id'); //สถานะการใช้งานครุภัณฑ์ รับค่าจากตาราง Asset_use_statuses
+            $table->string('brand'); //ยี่ห้อ
+            $table->string('model'); //รุ่น
+            $table->string('serial_no'); //หมายเลขประจำเครื่อง
+            $table->integer('display_type_id'); //ชนิดของจอภาพ รับค่าจากตาราง DisplayType
+            $table->integer('display_size'); //ขนาดจอภาพ
+            $table->integer('display_ratio_id'); //สัดส่วนจอภาพ รับค่าจากตาราง DisplayRatio
+            $table->boolean('is_vga')->default(0); //รองรับสัญญาณภาพ VGA
+            $table->boolean('is_dvi')->default(0); //รองรับสัญญาณภาพ DVI
+            $table->boolean('is_hdmi')->default(0); //รองรับสัญญาณภาพ HDMI
+            $table->boolean('is_dp')->default(0); //รองรับสัญญาณภาพ DisplayPort
+            $table->string('issues')->nullable(); //ปัญหาในการใช้งาน
+            $table->string('remarks')->nullable(); //หมายเเหตุ
             $table->timestamps();
         });
     }
