4758b89413016aa0b3ea7d58ea7aba45a125f36a
theaf1 <sophon.nit@mahidol.ac.th>
2021-09-13 13:32:56 +0700

    add role_id column (preliminary)

--- database/migrations/2021_08_23_084329_users.php
+++ database/migrations/2021_08_23_084329_users.php
@@ -20,6 +20,7 @@ class Users extends Migration
             $table->string('password')->bcrypt();
             $table->string('remember_token')->bcrypt()->nullable();
             $table->dateTime('email_verified_at')->nullable();
+            //$table->integer('role_id')->default(1);
             $table->timestamps();
         });
     }
