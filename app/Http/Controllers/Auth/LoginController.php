0b7b1380fd12b7f1c5ce9dec6ea34e85ca9037a8
theaf1 <sophon.nit@mahidol.ac.th>
2021-12-07 14:21:09 +0700

    translated login error message

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -60,7 +60,7 @@ class LoginController extends Controller
         }
 
         return back()->withErrors([
-            'username' => 'The provided credentials do not match our records.',
+            'username' => 'กรุณาตรวจสอบชื่อและรหัสผ่านให้ถูกต้อง',
         ]);
     }
 
