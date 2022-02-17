f3b772bb9dad9dde652c335f77cda56a03408b22
theaf1 <sophon.nit@mahidol.ac.th>
2022-02-17 15:03:55 +0700

    work on password reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 class ResetPasswordController extends Controller
 {
     /*
@@ -32,7 +34,23 @@ class ResetPasswordController extends Controller
     }
     public function execute()
     {
-       return $request->all();
+        $this->validate_reset($request);
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+     
+                $user->save();
+     
+                event(new PasswordReset($user));
+            }
+        );
+     
+        return $status === Password::PASSWORD_RESET
+                    ? redirect()->route('login')->with('status', __($status))
+                    : back()->withErrors(['email' => [__($status)]]);
     }
     private function validate_reset ($data)
     {
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('ตั้งรหัสผ่านใหม่') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
+                    <form method="POST" action="{{ url('/execute') }}">
                         @csrf
 
                         <input type="hidden" name="token" value="{{ $token }}">
@@ -49,7 +49,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('ตั้งรหัสผ่านใหม่') }}
