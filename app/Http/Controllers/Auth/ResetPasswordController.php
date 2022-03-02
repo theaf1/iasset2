691f2bc319c4b7a238bf214e69d7e605e3c015f5
theaf1 <sophon.nit@mahidol.ac.th>
2022-03-02 10:56:33 +0700

    added secret question feature (in work)

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -38,29 +38,30 @@ class ResetPasswordController extends Controller
     {
        
         //$this->validate_reset($request);
-        return $request->all(); //troubleshooting
-        // $status = Password::reset(
-        //     $request->only('email', 'password', 'password_confirmation', 'token'),
-        //     function ($user, $password) {
-        //         $user->forceFill([
-        //             'password' => Hash::make($password)
-        //         ])->setRememberToken(Str::random(60));
+        //return $request->all(); //troubleshooting
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
      
-        //         $user->save();
+                $user->save();
      
-        //         event(new PasswordReset($user));
-        //     }
-        // );
+                event(new PasswordReset($user));
+            }
+        );
      
-        // return $status === Password::PASSWORD_RESET
-        //             ? redirect()->route('login')->with('status', __($status))
-        //             : back()->withErrors(['email' => [__($status)]]);
+        return $status === Password::PASSWORD_RESET
+                    ? redirect()->route('login')->with('status', __($status))
+                    : back()->withErrors(['email' => [__($status)]]);
     }
     private function validate_reset ($data)
     {
         $rules = [
             'email'=>'required|email',
             'password'=>'required|min:8|confirmed',
+            'security_a'=>'required',
         ];
 
         $message = [
@@ -69,6 +70,7 @@ class ResetPasswordController extends Controller
             'password.required'=>'3',
             'password.min'=>'4',
             'password.confirmed'=>'5',
+            'security_a.required'=>'6'
         ];
         return $this->validate($data, $rules, $message);
     }
