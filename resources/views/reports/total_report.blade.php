770873cfae1b86fb1b48d3fadc7cc67161955805
theaf1 <sophon.nit@mahidol.ac.th>
2022-03-11 13:29:40 +0700

    remove bad code

--- resources/views/reports/total_report.blade.php
+++ resources/views/reports/total_report.blade.php
@@ -35,7 +35,7 @@
                 </tr>
             </tbody>
         </table>
-        <p class="text-center">ออกรายงาน ณ วันที่ {{$now}} โดย {{Auth::user()->name}}</p>
+        <p class="text-center">ออกรายงาน ณ วันที่ {{$now}} โดย</p>
         <div class="text-center">
             <a href="{{url('/reports')}}" class="btn btn-primary btn-lg mb-4" role="button">ย้อนกลับ</a>
         </div>
