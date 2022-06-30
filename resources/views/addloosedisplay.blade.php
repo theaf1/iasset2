5bff89c719995ffb8089860824f31589e0b821f8
theaf1 <sophon.nit@mahidol.ac.th>
2022-06-30 15:45:51 +0700

    add error alert

--- resources/views/addloosedisplay.blade.php
+++ resources/views/addloosedisplay.blade.php
@@ -8,6 +8,15 @@
                     {{ $message }}
                 </div>
             @endif
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <form action="{{url('/add-loosedisplay')}}" method="post">
                 @csrf
                 <div class="card mt-4">
