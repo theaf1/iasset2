5e82458edb35864ebcdd8d3ba1c2598071421999
theaf1 <sophon.nit@mahidol.ac.th>
2022-08-08 09:31:29 +0700

    work on thai langauge

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -19,7 +19,17 @@ class PdfController extends Controller
             ['name'=>'irene'],
         );
         //return view('/pdf/test');
-        $pdf=PDF::loadView('pdf.test',$data);
+        // $pdf=PDF::loadView('pdf.test',$data);
+        // return $pdf->stream('test.pdf');
+        $pdf=PDF::loadHTML('
+            <head>
+                <meta http-equiv="Content-Type" content="text/html"; charset=utf-8/>
+            </head>
+            <body>
+                <h1>ทดสอบ</h1>
+                <p>test<p>
+            </body>
+        ');
         return $pdf->stream('test.pdf');
     }
 
